d003dd65473f399ad22864da6b677f4e2dca61bc
leannapancoast <59525398+leannapancoast@users.noreply.github.com>
2024-09-06 00:49:53 -0700

    add status condition text box

--- makeChars.js
+++ makeChars.js
@@ -3,13 +3,14 @@ let numCharacters = 0;
 let numId = 0;
 localStorage.setItem('listOfCharacters', JSON.stringify(characters));
 localStorage.setItem('numCharacters', numCharacters);
-//console.log(localStorage);
+
 
 function map_range(value, low1, high1, low2, high2) {
     return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
 }
 function changeHealth(ele){
     let currChar = ele.parentElement;
+    console.log(currChar.id);
     let currHealth = document.getElementById("currHealthNum"+currChar.id).value;
     let maxHealth = document.getElementById("maxHealthNum"+currChar.id).value;
     let healthRatio = (currHealth/maxHealth)*100;
@@ -30,6 +31,21 @@ function changeHealth(ele){
     // localStorage.setItem('solari-health', healthRatio);
     // //console.log(healthLedOn);
     // document.getElementById('solari-health-bar').style.width= ""+(healthRatio)+"%";
+}
+
+function changeCondition(ele){
+    console.log('whee changing condition');
+    let currChar = ele.parentElement.parentElement;
+    let currCondition = ele.value;
+    if(currCondition != ''){
+        ele.style.backgroundColor = 'orange';
+    }
+    else{
+        // ele.style = 'background-color: none;';
+        ele.style.backgroundColor = 'white';
+    }
+    characters[currChar.id].condition = currCondition;
+    localStorage.setItem(currChar.id+'condition', currCondition);
 }
 
 let charDiv = document.getElementById('character-list');
@@ -61,7 +77,7 @@ function addCharacter(){
     currHealthLabel.id="currHealthLabel"+numId;
     let currHealthNum = document.createElement("input");
     currHealthNum.className = "curr-health-input";
-    currHealthNum.id="currHealthNum"+"char" +numId;
+    currHealthNum.id="currHealthNum"+"char"+numId;
     currHealthNum.type="number";
     currHealthNum.value=50;
     currHealthNum.min=0;
@@ -99,13 +115,25 @@ function addCharacter(){
     // currChar.append(breakElement.cloneNode(true));
     currHealthNum.setAttribute("onchange","changeHealth(this)");
     maxHealthNum.setAttribute("onchange","changeHealth(this)");
-    
+
+    let conditionGroup = document.createElement("div");
+    conditionGroup.className = 'condition-group';
+    let conditionLabel = document.createElement("label");
+    conditionLabel.className = 'condition-label';
+    conditionLabel.innerText = newName + ' Condition(s): ';
+    let conditionText = document.createElement("input");
+    conditionText.className = 'condition-text';
+    conditionText.id = 'conditionText' + 'char' + numId;
+    conditionText.setAttribute("onchange","changeCondition(this)");
+    conditionGroup.append(conditionLabel, conditionText);
+    currChar.append(conditionGroup);
     charDiv.append(currChar);
     
     let charJSON = {
         name: newName,
         currHealth: 50,
         maxHealth: 50,
+        condition: 'None'
     }
     characters['char'+numId]=charJSON;
     let deleteList = document.getElementById('deleteList');
@@ -116,6 +144,7 @@ function addCharacter(){
     numCharacters++;
     numId++;
     changeHealth(currHealthNum);
+    changeCondition(conditionText);
     localStorage.setItem('listOfCharacters', JSON.stringify(characters));
     localStorage.setItem('numCharacters', numCharacters);
     //console.log(characters);
